01660315bc7d29ac259a0abdeed2ce8e8483af4b
iamjesus <d.fox.fedra@gmail.com>
2024-01-06 17:57:53 -0500

    chore(ecommerce): Init project setup

---
+++ ecommerce/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '../styles/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Ecommerce',
+  description: 'Ecommerce with sanity and stripe',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
