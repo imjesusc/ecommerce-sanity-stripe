5822983ae3980c021638659bafdcca4e05f31b89
imjesusc <iamjesus.ds@gmail.com>
2024-01-06 22:10:51 -0500

    refactor(ecommerce): Change heroBanner styles to tailwind class

--- ecommerce/src/components/organisms/HeroBanner.tsx
+++ ecommerce/src/components/organisms/HeroBanner.tsx
@@ -8,19 +8,22 @@ export const HeroBanner = ({bannerData}: HeroBannerProps) => {
 const {smallText, midText, largeText1, product, image, desc, buttonText} = bannerData
 
   return (
-    <div className='hero-banner-container container m-auto'>
-    <div>
-      <p className='beats-solo'>{smallText}</p>
-      <h3>{midText}</h3>
-      <h1>{largeText1}</h1>
-      <Image src={image} alt='headphones' className='hero-banner-image'
+    <div className='py-20 px-10 bg-gray-200 rounded-lg relative h-[500px] leading-normal w-full container m-auto'>
+    <div className='grid gap-1'>
+      <p className='text-base tablet:text-2xl '>{smallText}</p>
+      <h3 className='text-3xl tablet:text-6xl mt-1 font-bold'>{midText}</h3>
+      <h1 className='text-white text-8xl uppercase font-bold tablet:text-[150px]'>{largeText1}</h1>
+      <figure className='absolute -top-12 -right-10 w-[300px] h-[300px] tablet:right-[20%] tablet:w-[450px] tablet:h-[450px] aspect-square'>
+      <Image src={image} alt={smallText} className='w-full h-full object-cover'
      width={500} height={500}/>
-      <div>
-        <Link href={`product/${product}`}>
-          <button type='button'>{buttonText}</button>
+      </figure>
+      <div className='mt-5'>
+        <Link href={`product/${product}`} className='rounded-lg py-2.5 px-4 bg-[#f02d34] text-white border-0 mt-10 text-lg font-medium cursor-pointer z-10'>
+          {buttonText}
         </Link>
-        <div className='desc'>
-          <h5>Description</h5>
+        
+        <div className='absolute right-10 bottom-5 w-[300px] leading-4 flex flex-col text-gray-500 gap-2 text-end'>
+          <h5 className='text-gray-600'>Description</h5>
           <p>{desc}</p>
         </div>
       </div>
