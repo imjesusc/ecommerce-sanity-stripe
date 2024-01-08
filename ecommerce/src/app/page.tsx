b03a8a08de9361fc0a49fe3c3ac59c322ffa8ae5
imjesusc <iamjesus.ds@gmail.com>
2024-01-08 00:54:10 -0500

    refactor(ecommerce): Remove styles from global styles and replace by tailwind class

--- ecommerce/src/app/page.tsx
+++ ecommerce/src/app/page.tsx
@@ -1,18 +1,18 @@
-import { FooterBanner, HeroBanner, ProductsGallery } from "@/components/organisms";
-import { getBannerData, getProductsData } from "@/utils/sanity/client";
+import { FooterBanner, HeroBanner, ProductsGallery } from '@/components/organisms'
+import { getBannerData, getProductsData } from '@/utils/sanity/client'
 
 export default async function HomePage() {
   const bannerData = await getBannerData()
   const productsData = await getProductsData()
   return (
-  <main>
-   <HeroBanner bannerData={bannerData ?? {}} />
-      <div className="products-heading">
-        <h2>Beset Selling Products</h2>
-        <p>Speakers of many variations</p>
+    <main>
+      <HeroBanner bannerData={bannerData ?? {}} />
+      <div className="text-center my-10 gap-4">
+        <h2 className="text-3xl text-gray-600">Beset Selling Products</h2>
+        <p className="text-gray-400">Speakers of many variations</p>
       </div>
-     <ProductsGallery productsData={productsData ?? []} />
-   <FooterBanner footerBannerData={bannerData ?? {}}/>
-  </main>
+      <ProductsGallery productsData={productsData ?? []} />
+      <FooterBanner footerBannerData={bannerData ?? {}} />
+    </main>
   )
 }
