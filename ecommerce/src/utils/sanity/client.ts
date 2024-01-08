a569ac54a61bc13fcdaaeef48ac695400b4f4379
imjesusc <iamjesus.ds@gmail.com>
2024-01-08 10:56:46 -0500

    update env variables

--- ecommerce/src/utils/sanity/client.ts
+++ ecommerce/src/utils/sanity/client.ts
@@ -1,9 +1,9 @@
 // ./src/utils/sanity/client.ts
 import { createClient, groq } from 'next-sanity'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-03'
+const projectId = '9uya2qmi'
+const dataset = 'production'
+const apiVersion = '2024-01-06'
 const token = process.env.NEXT_PUBLIC_SANITY_API_TOKEN
 
 const client = createClient({
