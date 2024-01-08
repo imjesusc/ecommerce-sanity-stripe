e7d546edca9ce477c36a56527c2e2853de5e9ad0
imjesusc <iamjesus.ds@gmail.com>
2024-01-08 15:46:27 -0500

    refactor(ecommerce): Added types to app Stripe api

--- ecommerce/src/app/api/stripe/route.ts
+++ ecommerce/src/app/api/stripe/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
-type Product = {
+interface Product {
   name: string
   image: {
     url: string
@@ -9,18 +9,18 @@ type Product = {
   quantity: number
 }
 
-export async function POST(request: Request) {
+export async function POST (request: Request): Promise<NextResponse> {
   const data: Product[] = await request.json()
-  const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY as string)
+  const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY ?? '')
 
-  const params = {
+  const params: Stripe.Checkout.SessionCreateParams = {
     submit_type: 'pay',
     mode: 'payment',
     payment_method_types: ['card'],
     billing_address_collection: 'auto',
     shipping_options: [
       { shipping_rate: 'shr_1OVLeEBY6kIj4T21ME5oDdyh' },
-      { shipping_rate: 'shr_1OWDTNBY6kIj4T21ElVpeicC' },
+      { shipping_rate: 'shr_1OWDTNBY6kIj4T21ElVpeicC' }
     ],
     line_items: data.map((item: Product) => {
       return {
@@ -28,21 +28,21 @@ export async function POST(request: Request) {
           currency: 'usd',
           product_data: {
             name: item.name,
-            images: [item.image.url],
+            images: [item.image.url]
           },
-          unit_amount: item.price * 100,
+          unit_amount: item.price * 100
         },
         adjustable_quantity: {
           enabled: true,
-          minimum: 1,
+          minimum: 1
         },
-        quantity: item.quantity,
+        quantity: item.quantity
       }
     }),
     success_url: `${request.headers.get('origin')}/success`,
-    cancel_url: `${request.headers.get('origin')}/`,
+    cancel_url: `${request.headers.get('origin')}/`
   }
 
-  const session = await stripe.checkout.sessions.create(params as any)
+  const session = await stripe.checkout.sessions.create(params)
   return NextResponse.json({ id: session.id })
 }
